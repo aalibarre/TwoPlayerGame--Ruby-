13578a9fefb7d2f6f3c958c601320cdd8a2664a3
Asha Alibarre <aalibarre@gmail.com>
2022-01-04 20:38:03 -0500

    rest of game logic

--- main.rb
+++ main.rb
@@ -13,3 +13,27 @@ player2 = Player.new(name2)
 
 # Current Player
 game = Game.new(player1, player2)
+while game.players[0].life > 0 game.players[1].life > 0 do 
+
+    # Question Generator  
+    puts "--- New Turn ---"
+    question = Question.new
+    print "#{game.current_player.name}: #{question.make_question}"
+    answer = question.answer
+  
+    # Input from player 
+    input = gets.chomp.to_i
+  
+    # Verify Answer 
+    if (input != answer)
+      game.subtract_life
+    end
+  
+    game.status_num_of_lives
+  
+    #Change Turn 
+    game.change_player
+  
+  
+  end 
+  
